5311855a68c92dfc0607cef47eae4ca69d592a08
Sajid Kamal <sajid.kamal@stonybrook.edu>
2017-02-26 20:27:56 -0500

    eliza is functional

--- public/javascripts/controllers/ElizaCtrl.js
+++ public/javascripts/controllers/ElizaCtrl.js
@@ -15,16 +15,27 @@ angular.module('appApp', []).controller('ElizaCtrl', function ($scope, $http) {
 
    	 $scope.sendResponse = function () {
 
-    	$http.post('/eliza/DOCTOR', $scope.human)
-   		.then(
-       		function(response){
-       			$scope.responses.push(response.data);
 
-       		},
-       	 function(response){
-         		$scope.responses.push("HELLO");
-       		}
-    	 ); 
+   	 	 $http({
+          method  : 'POST',
+          url     : '/eliza/DOCTOR',
+          data    : $scope.human, //forms user object
+          headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+         })
+          .success(function(data) {
+ 				$scope.responses.push(data);
+          });
+
+    	// $http.post('/eliza/DOCTOR', $scope.human)
+   		// .then(
+     //   		function(response){
+     //   			$scope.responses.push(response.data);
+
+     //   		},
+     //   	 function(response){
+     //     		$scope.responses.push("HELLO");
+     //   		}
+    	//  ); 
 
 
 

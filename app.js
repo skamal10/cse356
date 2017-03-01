125555cab034dbe99198a2c7a69eb74a654b04f7
root <root@skamal.cse356.compas.cs.stonybrook.edu>
2017-02-28 19:59:00 -0500

    db is working

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
-
-
-
+var db = require('./model/db');
 
 var index = require('./routes/index');
 var users = require('./routes/users');

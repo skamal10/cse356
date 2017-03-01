3952e8c125ed84b49506758a159c21535c78e3cd
Sajid Kamal <sajid.kamal@stonybrook.edu>
2017-02-28 23:25:22 -0500

    almost done

--- public/javascripts/controllers/ElizaCtrl.js
+++ public/javascripts/controllers/ElizaCtrl.js
@@ -67,7 +67,7 @@ angular.module('appApp', []).controller('ElizaCtrl', function ($scope, $http) {
 
                   var convo = {};
                   convo.id = data[i].convo_id;
-                  convo.date = data[i].date;
+                  convo.date = new Date(data[i].date);
                   $scope.convo_list.push( convo );
               }
           });

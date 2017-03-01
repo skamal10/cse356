39c3f21e03be5d00a6c5c4c3d7db53088b5c9d74
Sajid Kamal <sajid.kamal@stonybrook.edu>
2017-03-01 17:44:18 -0500

    alter db

--- model/Eliza.js
+++ model/Eliza.js
@@ -1,8 +1,17 @@
 var mongoose = require('mongoose');  
+var autoIncrement = require('mongoose-auto-increment');
+autoIncrement.initialize(mongoose);
+
+
+
+
+
 var elizaSchema = new mongoose.Schema({  
   user_id: Number,
-  convo_id: Number,
   date: { type: Date, default: Date.now },
   convo : { type : Array , "default" : [] }
 });
+
+
+elizaSchema.plugin(autoIncrement.plugin, 'Convo');
 mongoose.model('Convo', elizaSchema);

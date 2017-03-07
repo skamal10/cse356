08bf091379c7a206d04872f2272dd9f76e53ea5e
Sajid Kamal <sajidkamal@Sajids-MacBook-Air.local>
2017-03-07 18:44:44 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var amqp = require('amqplib/callback_api');
 
 
 
@@ -19,9 +19,48 @@ router.get('/eliza', function(req, res, next) {
 router.post('/listen',function(req,res,next) {
 
 
+
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'hw3';
+
+    ch.assertExchange(ex, 'direct', {durable: false});
+
+    ch.assertQueue('', {exclusive: true}, function(err, q) {
+      console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+      req.body['keys'].forEach(function(severity) {
+        ch.bindQueue(q.queue, ex, severity);
+      });
+
+      ch.consume(q.queue, function(msg) {
+        res.send(msg);
+      }, {noAck: true});
+    });
+  });
+});
+
 });
 
 router.post('/speak',function(req, res, next){
+
+	var amqp = require('amqplib/callback_api');
+
+	amqp.connect('amqp://localhost', function(err, conn) {
+  		conn.createChannel(function(err, ch) {
+    		var ex = 'hw3';
+    		var args = process.argv.slice(2);
+    		var msg = args.slice(1).join(' ') || 'Hello World!';
+    		var severity = (args.length > 0) ? args[0] : 'info';
+
+    		ch.assertExchange(ex, 'direct', {durable: false});
+    		ch.publish(ex, req.body['key'], new Buffer(req.body['msg']));
+    	
+  	});
+
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
 
 });
 
